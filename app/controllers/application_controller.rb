4732e5f1fbaf26eb60184a2f50cc28601ddf20d2
fufu717 <hiroyuki.kawai777@gmail.com>
2020-11-04 14:23:27 +0900

    ユーザー登録機能の実装
    ログイン、ログアウトまで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_day])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :email, :encrypted_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_day])
   end
 end
