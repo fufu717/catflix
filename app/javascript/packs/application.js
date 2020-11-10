f82a0e4029b6fa7d57ab258d3f7e82858264a333
fufu717 <hiroyuki.kawai777@gmail.com>
2020-11-10 12:55:02 +0900

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require ("trix")
+
 
 import 'bootstrap'
 import '../src/application.scss'
